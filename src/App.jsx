17a4ab1746347b4da20bb487ea53e21e503d51a2
madyanes <madyanes.dev@gmail.com>
2024-08-17 23:07:18 +0700

    Create dataReceived & dataFailed actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import { useEffect } from 'react'
+import { useEffect, useReducer } from 'react'
 import Header from './Header'
 import Main from './Main'
 
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      }
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      }
+    default:
+      throw new Error('Action unknown')
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   useEffect(() => {
     fetch('http://localhost:8000/questions')
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch(() => console.error('Error'))
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch(() => dispatch({ type: 'dataFailed' }))
   }, [])
 
   return (
