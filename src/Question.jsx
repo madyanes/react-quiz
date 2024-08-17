9c62c4860884f410c8c041356cb741f53412fcc1
madyanes <madyanes.dev@gmail.com>
2024-08-18 01:23:50 +0700

    Displaying questions

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,5 +1,14 @@
-function Question() {
-  return <div>Question</div>
+/* eslint-disable react/prop-types */
+
+import Options from './components/Options'
+
+function Question({ question }) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Options question={question} />
+    </div>
+  )
 }
 
 export default Question
