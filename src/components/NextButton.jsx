483aabd8dccb12480b80883362a3f733ea7cb1e7
madyanes <madyanes.dev@gmail.com>
2024-08-18 12:36:23 +0700

    Moving to the next question

---
+++ src/components/NextButton.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+
+function NextButton({ dispatch, answer }) {
+  if (answer === null) return null
+
+  return (
+    <div
+      className='btn btn-ui'
+      onClick={() => dispatch({ type: 'nextQuestion' })}
+    >
+      Next
+    </div>
+  )
+}
+
+export default NextButton
